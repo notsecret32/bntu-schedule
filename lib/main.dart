91bff41a81c37f20a721e634d50d8a60ca9c678d
notsecret32 <sashaskvorhevskiy@gmail.com>
2023-12-25 18:07:22 +0300

    feat: added logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
-  runApp(const BntuScheduleApp());
+  final Talker talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton(talker);
+
+  final Dio dio = Dio();
+  dio.interceptors.add(
+    TalkerDioLogger(
+      talker: talker,
+      settings: const TalkerDioLoggerSettings(
+        printResponseData: false,
+      ),
+    ),
+  );
+
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printStateFullData: false,
+      printEventFullData: false,
+    ),
+  );
+
+  FlutterError.onError = (FlutterErrorDetails details) =>
+      GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  runZonedGuarded(() => runApp(const BntuScheduleApp()),
+      (Object e, StackTrace st) {
+    GetIt.I<Talker>().handle(e, st);
+  });
 }
 
 class BntuScheduleApp extends StatelessWidget {
