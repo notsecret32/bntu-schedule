3ad2d26bbe6015b3cc7f1d195cb79cf38a789271
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-10 16:11:48 +0300

    refactor: firebase instances are temporarily removed

--- lib/src/core/firebase/firebase.dart
+++ lib/src/core/firebase/firebase.dart
@@ -1,7 +1,5 @@
 import 'package:bntu_schedule/src/config/firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> initializeFirebaseApp() async {
@@ -17,7 +15,3 @@ Future<void> initializeFirebaseApp() async {
     appleProvider: AppleProvider.appAttest,
   );
 }
-
-final FirebaseAuth auth = FirebaseAuth.instance;
-final FirebaseAppCheck appCheck = FirebaseAppCheck.instance;
-final FirebaseFirestore db = FirebaseFirestore.instance;
