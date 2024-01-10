0288c7d9dca6be377424e24a076c16e1146015e0
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-10 16:10:11 +0300

    refactor: the code has been refactored

--- lib/src/presentations/home/pages/home_page.dart
+++ lib/src/presentations/home/pages/home_page.dart
@@ -1,12 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:bntu_schedule/src/core/router/router.dart';
+import 'package:bntu_schedule/src/core/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
     return SafeArea(
       child: AutoTabsRouter(
         routes: const <PageRouteInfo>[
@@ -19,55 +19,19 @@ class HomePage extends StatelessWidget {
           final TabsRouter tabsRouter = AutoTabsRouter.of(context);
           final StackRouter router = AutoRouter.of(context);
           return Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                icon: const Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                  weight: 30,
-                ),
-                onPressed: () {
-                  router.navigateNamed('/settings');
-                },
-              ),
-              title: Text(
-                'Расписание',
-                style: theme.textTheme.headlineLarge,
-              ),
+            appBar: CustomAppBar(
+              title: 'Расписание',
+              icon: Icons.settings,
+              onPress: () async => await router.pushNamed('/settings'),
             ),
             body: child,
-            bottomNavigationBar: BottomNavigationBar(
-              showUnselectedLabels: true,
-              selectedItemColor: theme.primaryColor,
-              unselectedItemColor: theme.hintColor,
-              currentIndex: tabsRouter.activeIndex,
-              onTap: (int index) => _openPage(index, tabsRouter),
-              items: <BottomNavigationBarItem>[
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.schedule),
-                  label: 'Расписание',
-                ),
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.people),
-                  label: 'Преподаватели',
-                ),
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.assignment_sharp),
-                  label: 'Экзамены',
-                ),
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.newspaper),
-                  label: 'Новости',
-                ),
-              ],
+            bottomNavigationBar: CustomBottomNavigationBar(
+              router: router,
+              tabsRouter: tabsRouter,
             ),
           );
         },
       ),
     );
-  }
-
-  void _openPage(int index, TabsRouter tabsRouter) {
-    tabsRouter.setActiveIndex(index);
   }
 }
--- lib/src/presentations/settings/pages/settings_page.dart
+++ lib/src/presentations/settings/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:bntu_schedule/src/core/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -6,21 +7,12 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final StackRouter appRouter = AutoRouter.of(context);
-    final ThemeData theme = Theme.of(context);
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-            onPressed: appRouter.pop,
-          ),
-          title: Text(
-            'Настройки',
-            style: theme.textTheme.headlineLarge,
-          ),
+        appBar: CustomAppBar(
+          title: 'Настройки',
+          icon: Icons.arrow_back,
+          onPress: appRouter.pop,
         ),
         body: const Center(
           child: Text('Страница Настройки'),
