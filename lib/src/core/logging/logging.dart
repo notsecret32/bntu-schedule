f73268b60860b348c6a4127d902b74e22b12f5f5
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-10 14:12:37 +0300

    refactor: unnecessary code has been removed

--- lib/src/core/logging/logging.dart
+++ lib/src/core/logging/logging.dart
@@ -1,25 +1,13 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger.dart';
-import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 Future<void> initializeLoggers() async {
   final Talker talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug('Talker started...');
-
-  final Dio dio = Dio();
-  dio.interceptors.add(
-    TalkerDioLogger(
-      talker: talker,
-      settings: const TalkerDioLoggerSettings(
-        printResponseData: false,
-      ),
-    ),
-  );
 
   Bloc.observer = TalkerBlocObserver(
     talker: talker,
