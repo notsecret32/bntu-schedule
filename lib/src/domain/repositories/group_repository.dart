fb0d3715203b930a541830d54229cc3e0be8e3af
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 13:11:27 +0300

    feat: repositories for working with the group have been added

---
+++ lib/src/domain/repositories/group_repository.dart
@@ -0,0 +1,28 @@
+import 'package:bntu_schedule/src/core/error/error.dart';
+import 'package:bntu_schedule/src/domain/entities/group_number_entity.dart';
+import 'package:dartz/dartz.dart';
+
+/// An abstract class for working with groups.
+///
+/// This class allows you to:
+/// * Set the selected group
+/// * Remove the selected group
+/// * Get the numbers of all available groups
+abstract class GroupRepository {
+  /// Sets the selected group in the cache.
+  ///
+  /// If an error occurs, the [CacheFailure] class returns.
+  Future<Either<Failure, void>> setGroupNumber(
+    String number,
+  );
+
+  /// Removes the selected group from the cache.
+  ///
+  /// If an error occurs, the [CacheFailure] class returns.
+  Future<Either<Failure, void>> removeSelectedGroup();
+
+  /// Gets all the group numbers that are in the database.
+  ///
+  /// Returns [ServerFailure] or [CacheFailure] if an error has occurred.
+  Future<Either<Failure, List<GroupNumberEntity>>> getAllGroupsNumber();
+}
