d3a2c0da4d15aa5b6e4942e150832d460a8ed3c5
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 13:08:32 +0300

    docs: added documentation for `CustomToggleText`

--- lib/src/core/widgets/custom_toggle_text.dart
+++ lib/src/core/widgets/custom_toggle_text.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-class ToggleText extends StatelessWidget {
-  const ToggleText({
+/// Wrapper over the [Text] widget.
+///
+/// Used to switch the [TextStyle] between two options.
+class CustomToggleText extends StatelessWidget {
+  const CustomToggleText({
     super.key,
     required this.text,
     required this.activeTextStyle,
@@ -9,9 +12,16 @@ class ToggleText extends StatelessWidget {
     this.isActive = false,
   });
 
+  /// Displayed text.
   final String text;
+
+  /// The [TextStyle] when the widget is activated.
   final TextStyle activeTextStyle;
+
+  /// The [TextStyle] when the widget is inactivated.
   final TextStyle inactiveTextStyle;
+
+  /// Switches [TextStyle].
   final bool isActive;
 
   @override
