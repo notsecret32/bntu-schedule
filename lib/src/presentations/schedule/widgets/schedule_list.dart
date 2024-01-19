a8a68ccebce93568bb15e14f9be17011955e2478
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 13:18:39 +0300

    refactor: the 'ScheduleList` widget has been refactored

--- lib/src/presentations/schedule/widgets/schedule_list.dart
+++ lib/src/presentations/schedule/widgets/schedule_list.dart
@@ -1,17 +1,19 @@
 import 'package:bntu_schedule/src/core/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
+/// A widget for convenient display of several lessons.
 class ScheduleList extends StatelessWidget {
   const ScheduleList({
     super.key,
     required this.items,
   });
 
+  /// A list of widgets to display.
   final List<Widget> items;
 
   @override
   Widget build(BuildContext context) {
-    return BaseContainer(
+    return CustomContainer(
       child: Column(
         children: items,
       ),
