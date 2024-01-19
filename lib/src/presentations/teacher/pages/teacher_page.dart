20f565da95a126163ace9a323476beb31af4005d
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 13:15:33 +0300

    fix: the nested `Scaffold` has been removed

--- lib/src/presentations/teacher/pages/teacher_page.dart
+++ lib/src/presentations/teacher/pages/teacher_page.dart
@@ -5,10 +5,8 @@ import 'package:flutter/material.dart';
 class TeacherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Страница Преподаватель'),
-      ),
+    return const Center(
+      child: Text('Страница Преподаватель'),
     );
   }
 }
