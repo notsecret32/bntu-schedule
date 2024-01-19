d605ecd456dafd42659b54514b851102a1233ddb
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 12:58:10 +0300

    feat: Added a new `UseCase` & `NoParams` class

--- lib/src/core/usecases/usecase.dart
+++ lib/src/core/usecases/usecase.dart
@@ -1,6 +1,14 @@
-import 'package:bntu_schedule/src/core/error/failure.dart';
+import 'package:bntu_schedule/src/core/error/failure/failure.dart';
 import 'package:dartz/dartz.dart';
 
+export 'no_params.dart';
+
+/// An abstract class responsible for performing specific business operations,
+/// interacting with the outside world through the `Data` layer and interacting
+/// with the `Presentation` layer through the `Interface`.
+///
+/// Data validation takes place here, some comparisons, calculations and etc.
 abstract class UseCase<Type, Params> {
+  /// Calls [UseCase] with the passed parameters.
   Future<Either<Failure, Type>> call(Params params);
 }
