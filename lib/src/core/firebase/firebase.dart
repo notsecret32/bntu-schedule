afab52c2ac2be8e7cf9c037b3f5ac9b0cea539be
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 12:55:38 +0300

    docs: added documentation to some files

--- lib/src/core/firebase/firebase.dart
+++ lib/src/core/firebase/firebase.dart
@@ -2,6 +2,9 @@ import 'package:bntu_schedule/src/config/firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+/// Initializes the [Firebase] application.
+///
+/// This is also where the application and [Firebase] modules are configured.
 Future<void> initializeFirebaseApp() async {
   // App Initialization
   await Firebase.initializeApp(
