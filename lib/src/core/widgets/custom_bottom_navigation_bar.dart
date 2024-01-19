ac9e40f8e36c0537442a3fe7d5bd00572841b8e0
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-19 13:06:44 +0300

    refactor & docs: widget `CustomBottomNavigationBar` has been optimized & documented

--- lib/src/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/src/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+/// The default list of [BottomNavigationBarItem].
 const List<BottomNavigationBarItem> defaultBottomNavigationItems =
     <BottomNavigationBarItem>[
   BottomNavigationBarItem(
@@ -21,17 +22,19 @@ const List<BottomNavigationBarItem> defaultBottomNavigationItems =
   ),
 ];
 
+/// Wrapper over the [BottomNavigationBar] widget, used to minimize repetitive code.
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({
     super.key,
     required this.tabsRouter,
-    required this.router,
-    this.items = defaultBottomNavigationItems,
+    this.items,
   });
 
+  /// A controller for navigating between pages using the [BottomNavigationBar].
   final TabsRouter tabsRouter;
-  final StackRouter router;
-  final List<BottomNavigationBarItem> items;
+
+  /// List of buttons for the [BottomNavigationBar].
+  final List<BottomNavigationBarItem>? items;
 
   @override
   Widget build(BuildContext context) {
@@ -42,20 +45,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
       unselectedItemColor: theme.hintColor,
       currentIndex: tabsRouter.activeIndex,
       onTap: (int index) => _openPage(index, tabsRouter),
-      items: items
-          .map(
-            (BottomNavigationBarItem item) => BottomNavigationBarItem(
-              label: item.label,
-              icon: item.icon,
-              activeIcon: item.activeIcon,
-              backgroundColor: item.backgroundColor,
-              tooltip: item.tooltip,
-            ),
-          )
-          .toList(),
+      items: items == null ? defaultBottomNavigationItems : items!.toList(),
     );
   }
 
+  /// Opens the page by index.
   void _openPage(int index, TabsRouter tabsRouter) {
     tabsRouter.setActiveIndex(index);
   }
