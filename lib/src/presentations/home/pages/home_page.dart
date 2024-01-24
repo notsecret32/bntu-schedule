16d71396bb82988045a87a1cda8845761dc3d0b8
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-24 17:29:04 +0300

    feat: the appearance of the main page has been improved

--- lib/src/presentations/home/pages/home_page.dart
+++ lib/src/presentations/home/pages/home_page.dart
@@ -1,36 +1,38 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:bntu_schedule/src/core/router/router.dart';
-import 'package:bntu_schedule/src/core/widgets/widgets.dart';
+import 'package:bntu_schedule/src/core/widgets/custom_app_bar.dart';
+import 'package:bntu_schedule/src/core/widgets/custom_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: AutoTabsRouter(
-        routes: const <PageRouteInfo>[
-          ScheduleRoute(),
-          TeacherRoute(),
-          ExamRoute(),
-          NewsRoute(),
-        ],
-        builder: (BuildContext context, Widget child) {
-          final TabsRouter tabsRouter = AutoTabsRouter.of(context);
-          final StackRouter router = AutoRouter.of(context);
-          return Scaffold(
-            appBar: CustomAppBar(
-              title: 'Расписание',
-              icon: Icons.settings,
-              onPress: () async => await router.pushNamed('/settings'),
-            ),
-            body: child,
-            bottomNavigationBar: CustomBottomNavigationBar(
-              tabsRouter: tabsRouter,
-            ),
-          );
-        },
-      ),
+    return AutoTabsRouter.tabBar(
+      animatePageTransition: false,
+      routes: const <PageRouteInfo>[
+        ScheduleRoute(),
+        TeacherRoute(),
+        ExamRoute(),
+        NewsRoute(),
+        SettingsRoute(),
+      ],
+      builder: (
+        BuildContext context,
+        Widget child,
+        TabController tabController,
+      ) {
+        final TabsRouter autoTabsRouter = AutoTabsRouter.of(context);
+        return Scaffold(
+          appBar: const CustomAppBar(
+            title: 'БНТУ Расписание',
+          ),
+          body: child,
+          bottomNavigationBar: CustomBottomNavigationBar(
+            tabsRouter: autoTabsRouter,
+          ),
+        );
+      },
     );
   }
 }
