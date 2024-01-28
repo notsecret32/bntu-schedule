8389cbebac24bd9bbcb5079abe6a339b91130be7
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-28 22:28:54 +0300

    feat: added a `BottomSheet` for the Schedules page

--- lib/features/schedules/presentation/pages/schedules_page.dart
+++ lib/features/schedules/presentation/pages/schedules_page.dart
@@ -3,6 +3,7 @@ import 'package:bntu_schedule/core/constants/routes.dart';
 import 'package:bntu_schedule/core/widgets/widgets.dart';
 import 'package:bntu_schedule/features/schedules/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 @RoutePage()
 class SchedulesPage extends StatelessWidget {
@@ -17,8 +18,11 @@ class SchedulesPage extends StatelessWidget {
           homeSettingsPageRouteKey,
         ),
         actions: <CustomAppBarActionButton>[
-          const CustomAppBarActionButton(
+          CustomAppBarActionButton(
             icon: Icons.search,
+            onPressed: () {
+              _showBottomSheet(context);
+            },
           ),
         ],
       ),
@@ -56,6 +60,41 @@ class SchedulesPage extends StatelessWidget {
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showBottomSheet(BuildContext context) async {
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      enableDrag: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      context: context,
+      builder: (BuildContext context) => SingleChildScrollView(
+        child: Container(
+          padding: MediaQuery.of(context).viewInsets,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                CustomTextField(
+                  hintText: 'Введите номер группы',
+                  textFilter: FilteringTextInputFormatter.digitsOnly,
+                  textMaxLength: 8,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomButton(text: 'Выбрать'),
+              ],
+            ),
+          ),
         ),
       ),
     );
