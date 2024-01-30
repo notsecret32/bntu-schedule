720df44fb9e529060b47310bdc399a48c21a954b
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-01-30 03:04:23 +0300

    feat: added a widget for quick group selection

--- lib/features/schedules/presentation/widgets/widgets.dart
+++ lib/features/schedules/presentation/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'schedule_empty_item.dart';
 export 'schedule_item.dart';
 export 'schedule_list.dart';
+export 'select_group_bottom_sheet.dart';
 export 'week_calendar.dart';
 export 'weekly_date_picker.dart';
