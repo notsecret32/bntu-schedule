1fae444279edf8fe761775df3fe57bb9c903497d
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-02-20 16:31:17 +0300

    fix: fixed a bug in the admin panel
    Fixed a bug where it was impossible to log out of the admin panel

--- lib/features/admin/presentation/pages/admin_home_page.dart
+++ lib/features/admin/presentation/pages/admin_home_page.dart
@@ -1,11 +1,9 @@
-import 'package:bntu_schedule/core/router/routes_list.dart';
-import 'package:bntu_schedule/core/widgets/custom_app_bar.dart';
 import 'package:bntu_schedule/features/admin/presentation/cubit/admin_auth_cubit.dart';
-import 'package:bntu_schedule/features/admin/presentation/widgets/admin_home_body.dart';
 import 'package:bntu_schedule/injection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
+
+import './admin_home_view.dart';
 
 class AdminHomePage extends StatelessWidget {
   const AdminHomePage({super.key});
@@ -14,28 +12,7 @@ class AdminHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<AdminAuthCubit>(
       create: (BuildContext context) => sl<AdminAuthCubit>(),
-      child: BlocListener<AdminAuthCubit, AdminAuthState>(
-        listener: (BuildContext context, AdminAuthState state) {
-          if (state is AdminAuthLoggedOutState) {
-            context.goNamed(
-              RoutesList.adminAuthenticationPage.name,
-            );
-          }
-        },
-        child: Scaffold(
-          appBar: CustomAppBar(
-            title: 'Админ Панель',
-            leadingIcon: Icons.exit_to_app,
-            onLeadingIconPress: () async =>
-                await _onLogoutButtonPressed(context),
-          ),
-          body: const AdminHomeBody(),
-        ),
-      ),
+      child: const AdminHomeView(),
     );
-  }
-
-  Future<void> _onLogoutButtonPressed(BuildContext context) async {
-    await context.read<AdminAuthCubit>().logout();
   }
 }
