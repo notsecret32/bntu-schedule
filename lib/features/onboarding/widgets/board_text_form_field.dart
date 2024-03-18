3b93002cac4bf5a6b9ac07955bf2979db3e0ed90
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-03-18 23:37:00 +0300

    feat: added new parameters for `BoardTextFormField`

--- lib/features/onboarding/widgets/board_text_form_field.dart
+++ lib/features/onboarding/widgets/board_text_form_field.dart
@@ -5,23 +5,49 @@ import '../../../core/theme/colors/light_colors.dart';
 import '../../../core/theme/text_theme.dart';
 
 class BoardTextFormField extends StatelessWidget {
-  const BoardTextFormField({super.key});
+  const BoardTextFormField({
+    super.key,
+    this.maxLength = 8,
+    this.controller,
+    this.onChanged,
+    this.validator,
+  });
+
+  final int? maxLength;
+  final TextEditingController? controller;
+  final void Function(String value)? onChanged;
+  final String? Function(String? value)? validator;
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     return PlatformTextFormField(
+      // ========== [Validation] ==========
+      controller: controller,
+      validator: validator,
+      maxLength: maxLength,
+      onChanged: onChanged,
+
+      // ========== [Behavior] ==========
       autofocus: true,
+
+      // ========== [Material Style] ==========
       material: (_, __) => MaterialTextFormFieldData(
         style: textTheme.body,
         keyboardType: TextInputType.number,
+
+        // ========== [Decoration] ==========
         decoration: InputDecoration(
+          // ========== [Input] ==========
+          contentPadding: const EdgeInsets.all(16),
+
+          // ========== [Hint Style] ==========
           hintText: 'Номер группы',
           hintStyle: textTheme.body.copyWith(
             color: lightLabelSecondary,
           ),
-          focusColor: lightLabelQuaternary,
-          contentPadding: const EdgeInsets.all(16),
+
+          // ========== [Border Style] ==========
           border: const OutlineInputBorder(
             borderSide: BorderSide(
               color: lightLabelQuaternary,
@@ -30,6 +56,9 @@ class BoardTextFormField extends StatelessWidget {
               Radius.circular(16),
             ),
           ),
+
+          // ========== [Focused Border Style] ==========
+          focusColor: lightLabelQuaternary,
           focusedBorder: const OutlineInputBorder(
             borderSide: BorderSide(
               color: lightLabelQuaternary,
@@ -38,6 +67,9 @@ class BoardTextFormField extends StatelessWidget {
               Radius.circular(16),
             ),
           ),
+
+          // ========== [Error Style] ==========
+          errorMaxLines: 3,
         ),
       ),
     );
